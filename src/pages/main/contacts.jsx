28cf18035049864f3b77cc2a39ea2065f5c4a359
Toniodujinrin <todujinrin@gmail.com>
2023-08-30 11:26:33 -0600

    refactor

--- src/pages/main/contacts.jsx
+++ src/pages/main/contacts.jsx
@@ -1,4 +1,4 @@
-import React, {useContext, useEffect, useState} from 'react';
+import React, {useContext} from 'react';
 import Overview from '../../components/conversations/overview';
 import { UserContext } from '../../contexts/UserContext';
 import ContactsComp from '../../components/contacts';
