165fa1e17502e1cb5a30e6a9b187c8c08923b233
Toniodujinrin <todujinrin@gmail.com>
2023-09-14 20:12:31 -0600

    ui improvements

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -37,14 +37,14 @@ const Home = () => {
 
     return ( 
         <div className=' scroll-smooth'>
-         <nav className={`border-b-2  w-screen  bg-black z-30 fixed  border-midGray h-[70px] flex flex-row items-center justify-between p-3`}>
+         <nav className={`border-b-2  w-screen  bg-[#1d1438] z-30 fixed  border-black h-[70px] flex flex-row items-center justify-between p-3`}>
             <img className='w-[40px] h-[40px]' src="../../logo.svg" alt="" />
             <div className='flex flex-row items-center gap-4'>
             <ButtonMain text={"Get Started"} onClick={()=>navigate("/signup")}/>
             <ButtonMain text={"Login"} onClick={()=>navigate("/login")}/>
             </div>
         </nav>
-        <div className='w-screen overflow-hidden pt-[70px] flex flex-col items-center bg-black  min-h-screen'>
+        <div className='w-screen overflow-hidden pt-[70px] flex flex-col items-center mainGradient  min-h-screen'>
            
             
         <motion.div initial={{opacity:0, scale:0.8}} transition={{duration:0.3}} animate={{opacity:1, scale:1}} className='flex flex-col p-4 my-[100px]   justify-center items-start   gap-y-4 '>
